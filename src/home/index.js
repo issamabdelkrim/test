80a269584c43f7a785cebabfff4fbd4ee47a4602
abdelkrim.issam@gmail.com <abdelkrim.issam@gmail.com>
2020-03-22 22:44:24 +0100

    last commit

--- src/home/index.js
+++ src/home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import CalculPage from "../calcul";
-import ContactPage from "../contact/index";
+
 
 class HomePage extends Component {
   constructor(props) {
